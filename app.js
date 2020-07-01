02f11920a1b0d366f2715a29acbe537c5c5f42af
Anna Lee <annaleeart@yandex.ru>
2020-07-01 21:36:40 +0300

    Added limiter

--- app.js
+++ app.js
@@ -3,23 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const router = require('./routes/index');
 
 const app = express();
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const limiter = require('./middlewares/limiter');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT, MONGODB_URL } = require('./config');
 
 const { errorHandler } = require('./middlewares/error-handler');
-const router = require('./routes/index');
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
 
 mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
@@ -33,13 +29,11 @@ mongoose.connect(MONGODB_URL, {
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(helmet());
 app.use(limiter);
-
+app.use(helmet());
 app.use(requestLogger);
-app.use(router);
 
+app.use(router);
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
