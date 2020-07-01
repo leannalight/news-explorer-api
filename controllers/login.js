62d24b400c180589527ef958010c48c630c22c66
Anna Lee <annaleeart@yandex.ru>
2020-07-01 16:00:34 +0300

    user: user.omitPrivate()

--- controllers/login.js
+++ controllers/login.js
@@ -14,7 +14,7 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, email, password: hash,
     }))
-    .then((user) => res.status(201).send({ data: user.omitPrivate() }))
+    .then((user) => res.status(201).send({ user: user.omitPrivate() }))
     .catch(next);
 };
 
