eadf9e80e3afb847f5371bd7af8f58c1222bad8a
Anna Lee <annaleeart@yandex.ru>
2020-07-03 16:40:12 +0300

    Changed Port for 3033

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET, NODE_ENV, PORT, MONGODB_URL} = process.env;
 
-module.exports.PORT = PORT || 3000;
+module.exports.PORT = PORT || 3033;
 module.exports.MONGODB_URL = NODE_ENV !== 'production' ? 'mongodb://localhost:27017/newsdb' : MONGODB_URL;
 module.exports.PrivateKey = NODE_ENV !== 'production' ? 'some-dev-secret' : JWT_SECRET;
