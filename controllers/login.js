8c9e434de8114de15f16971451e881a32bff910b
Anna Lee <annaleeart@yandex.ru>
2020-08-17 18:12:52 +0300

    Fixed CORS

--- controllers/login.js
+++ controllers/login.js
@@ -16,7 +16,7 @@ module.exports.createUser = (req, res, next) => {
     .then((user) => res.status(201).send({ user: user.omitPrivate() }))
     .catch(next);
 };
-
+/*
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
@@ -31,4 +31,20 @@ module.exports.login = (req, res, next) => {
       return res.send({ token });
     })
     .catch(next);
+};
+*/
+module.exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const userlogin = await User.findUserByCredentials(email, password);
+    const token = jwt.sign({ _id: userlogin._id }, PrivateKey, { expiresIn: '7d' });
+    res.cookie('jwt', token, {
+      maxAge: 3600000 * 24 * 7,
+      httpOnly: true,
+      domain: 'explorenews.tk',
+    });
+    return res.send({ token });
+    } catch(error) {
+      return next(error);
+  }
 };
