d765f9f31ec87a5e57c69d0b55e68addb3a1a51c
Anna Lee <annaleeart@yandex.ru>
2020-08-17 17:54:40 +0300

    Added CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
@@ -7,7 +8,7 @@ const { errors } = require('celebrate');
 const app = express();
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const limiter = require('./middlewares/limiter');
+// const limiter = require('./middlewares/limiter');
 const router = require('./routes/index');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -15,6 +16,17 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGODB_URL } = require('./config');
 
 const { errorHandler } = require('./middlewares/error-handler');
+
+const corsOptions = {
+  origin:[
+    'https://explorenews.tk/',
+    'http://localhost:8080',
+    'https://leannalight.github.io/news-explorer-frontend',
+    'https://leannalight.github.io'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
@@ -26,7 +38,7 @@ mongoose.connect(MONGODB_URL, {
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(limiter);
+// app.use(limiter);
 app.use(helmet());
 app.use(requestLogger);
 
